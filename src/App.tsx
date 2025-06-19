1e1b7403f67b1be5764d91b0085c6fb9631c62a2
Amriad7 <amrouchmohamedriad@gmail.com>
2025-06-19 19:21:15 +0100

    remove react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import Header from "./components/header";
-import { Outlet } from "react-router";
+// import Header from "./components/header";
+// import { Outlet } from "react-router";
 import Home from "./pages/home";
 
 function App() {
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router";
+// import { createBrowserRouter, RouterProvider } from "react-router";
 import App from "./App.tsx";
-import Home from "./pages/home.tsx";
+// import Home from "./pages/home.tsx";
 import "./index.css";
 
-const router = createBrowserRouter([
+/*const router = createBrowserRouter([
   {
     path: "/",
     Component: App,
     children: [{ index: true, Component: Home }],
   },
-]);
+]);*/
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App/>
+    <App />
     {/* <RouterProvider router={router} /> */}
   </StrictMode>
 );
