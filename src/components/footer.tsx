58add24d228629daf1e61a2809166bcffdee2f15
Amriad7 <amrouchmohamedriad@gmail.com>
2025-06-29 19:39:08 +0100

    footer light theme fixed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,7 +7,9 @@ const Footer = () => {
       <Separator />
       <footer className="flex items-center justify-center h-[60px] sm:h-[80px] w-full">
         <P className="italic">
-          <span className="font-semibold text-gray-50">(c) 2025 Amriad.</span>{" "}
+          <span className="font-semibold text-foreground">
+            (c) 2025 Amriad.
+          </span>
           All rights reserved.
         </P>
       </footer>
