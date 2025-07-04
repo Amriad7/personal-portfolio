caa49efba84f52c607a66ff65a038d776489437e
Amriad7 <amrouchmohamedriad@gmail.com>
2025-07-04 16:11:00 +0100

    admin layout added

--- src/pages/admin/admin.tsx
+++ src/pages/admin/admin.tsx
@@ -1,10 +1,15 @@
+import Sidebar from "@/components/admin/side-bar";
 import { Outlet } from "react-router";
 
 const Admin = () => {
   return (
-    <div>
-      <h1>Admin Dashboard</h1>
-      <Outlet />
+    <div className="relative bg-background text-foreground font-nunito ">
+      <main className="flex justify-start items-stretch min-h-screen">
+        <Sidebar />
+        <div className="flex-1 ">
+          <Outlet />
+        </div>
+      </main>
     </div>
   );
 };
